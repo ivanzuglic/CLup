7e6c9d40948d203f08be1dfd5884a9e0a2ab877f
Luka Mrković <luka.mrkovic@fer.hr>
2020-11-10 12:48:37 +0100

    Working Hours table
    Added and tested

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
          $this->call(StoresTypesSeeder::class);
          $this->call(UsersTableAdminSeeder::class);
          $this->call(StoresSeeder::class);
+         $this->call(WorkingHoursTableSeeder::class);
     }
 }
