d89681c1aa652518d3ee12fd23a0aaa9e8f7b508
Ivan Zuglic <ivan.zuglic@fer.hr>
2020-11-30 19:58:05 +0100

    Added new stores in StoresSeeder

--- database/seeds/StoresSeeder.php
+++ database/seeds/StoresSeeder.php
@@ -37,7 +37,61 @@ class StoresSeeder extends Seeder
 
                 'max_occupancy' => '20',
                 'current_occupancy' => '0',
+            ],
+            [
+                'name' => 'Best Supermarket',
+                'description' => 'In whole world',
+                'store_type' => '1',
+                'image_reference' => 'https://clipartstation.com/wp-content/uploads/2017/11/supermarket-building-clipart.gif',
+                'address_line_1' => 'Alpha Supermarket Avenue 2',
+                'zip_code' => "20000",
+                'town' => "London",
+                'country' => 'Alpha',
+
+                'max_occupancy' => '1000',
+                'current_occupancy' => '800',
+            ],
+
+            [
+                'name' => 'Pharmacy AAP',
+                'description' => 'All kinds of drugs',
+                'store_type' => '2',
+                'image_reference' => 'https://image.freepik.com/free-vector/pharmacy-building-big-city-vector-illustration_61747-290.jpg',
+                'address_line_1' => 'Alpha Apothecary Avenue 41',
+                'zip_code' => "50000",
+                'town' => "Vladivostok",
+                'country' => 'Alpha',
+
+                'max_occupancy' => '90',
+                'current_occupancy' => '2',
+            ],
+            [
+                'name' => 'Best Served Cold',
+                'description' => 'Only one on whole continent',
+                'store_type' => '1',
+                'image_reference' => 'https://compote.slate.com/images/511985a0-36fd-450a-a6f5-59a53899cab3.jpg',
+                'address_line_1' => 'Frozen way 3',
+                'zip_code' => "30000",
+                'town' => "McMurdo Base Antarctica",
+                'country' => 'Antarctica',
+
+                'max_occupancy' => '20',
+                'current_occupancy' => '15',
+            ],
+            [
+                'name' => 'Big Bad Store',
+                'description' => 'I do not want to put image',
+                'store_type' => '1',
+                'image_reference' => '',
+                'address_line_1' => 'Alpha Supermarket Avenue 4',
+                'zip_code' => "20000",
+                'town' => "London",
+                'country' => 'Alpha',
+
+                'max_occupancy' => '100',
+                'current_occupancy' => '0',
             ]
+
         ];
 
         foreach ($stores as $store)
