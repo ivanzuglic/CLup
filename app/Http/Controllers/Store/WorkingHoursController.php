f8ba554f7ec4cb46573924fd5481b14830e0629a
Luka Mrković <luka.mrkovic@fer.hr>
2020-12-03 14:21:39 +0100

    Working Hours Fix

--- app/Http/Controllers/Store/WorkingHoursController.php
+++ app/Http/Controllers/Store/WorkingHoursController.php
@@ -6,6 +6,7 @@ use App\WorkingHours;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class WorkingHoursController extends Controller
 {
@@ -59,6 +60,68 @@ class WorkingHoursController extends Controller
     public function edit ()
     {
         // return edit field
+    }
+
+    // Bulk Create/Update/Delete function
+    public function bulk_CUD(Request $request, $store_id)
+    {
+        // Iteration for each day
+        for($day = 0; $day < 7; $day++)
+        {
+            // If there is an array with selected days present in the request and there is a current iterator day in it
+            if($request->has('days_selected') && in_array($day, $request->days_selected))
+            {
+                // If both opening hours and closing hours are defined
+                if($request->opening_hours[$day] != null && $request->closing_hours[$day] != null)
+                {
+                    // If entry for the iterating day already exists in the database
+                    if(WorkingHours::where([['day', '=', $day], ['store_id', '=', $store_id]])->exists())
+                    {
+                        $working_hours_update = WorkingHours::where([['day', '=', $day], ['store_id', '=', $store_id]])->firstOrFail();
+                        $working_hours_update->update([
+                            'opening_hours' => $request->opening_hours[$day],
+                            'closing_hours' => $request->closing_hours[$day],
+                        ]);
+                    }
+                    // Else, create a new entry
+                    else
+                    {
+                        WorkingHours::create([
+                            'store_id' => $store_id,
+                            'day' => $day,
+                            'opening_hours' => $request->opening_hours[$day],
+                            'closing_hours' => $request->closing_hours[$day],
+                        ]);
+                    }
+                }
+            }
+            else
+            {
+                // If entry for the iterating day exists in the database, but isn't selected
+                if(WorkingHours::where([['day', '=', $day], ['store_id', '=', $store_id]])->exists())
+                {
+                    $working_hours_delete = WorkingHours::where([['day', '=', $day], ['store_id', '=', $store_id]])->firstOrFail();
+                    $working_hours_delete->delete();
+                }
+            }
+        }
+
+//        if($request->has('days_selected'))
+//        {
+//            foreach ($request->days_selected as $day)
+//            {
+//               if($request->opening_hours[$day] != null && $request->closing_hours[$day] != null)
+//                {
+//                    DB::table('working_hours')
+//                        ->updateOrInsert(
+//                            ['store_id' => $store_id, 'day' => $day],
+//                            ['opening_hours' => $request->opening_hours[$day], 'closing_hours' => $request->closing_hours[$day]]
+//                        );
+//                }
+//            }
+//        }
+
+        return back();
     }
 
     public function update (Request $request, $store_id, $working_hours_id)
